6e916f0063f931c81f8c0a48c97d3ca50dd99359
Jarrod Flesch <jarrodmflesch@gmail.com>
2020-09-17 21:37:09 -0400

    adds sound clip

--- src/components/blocks/RandomImage/index.tsx
+++ src/components/blocks/RandomImage/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import useSound from 'use-sound';
+
 
 import './index.scss';
 
@@ -10,16 +12,20 @@ interface Image {
 interface RandomImageBlockProps {
   images: Image[],
 }
+const sound = 'http://localhost:3000/sounds/rise-climb-audio.mp3';
 
 const baseClass = 'random-image-block';
 
 export const RandomImageBlock: React.FC<RandomImageBlockProps> = (props) => {
   const { images } = props;
+  
+  const [play] = useSound(sound, { playbackRate: 0.75, volume: 1 });
 
   const [randomImage, setRandomImage] = useState<Image>(images[0]);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
 
   const chooseRandomly = () => {
+    play();
     setIsAnimating(true);
 
     setTimeout(() => {
@@ -33,7 +39,7 @@ export const RandomImageBlock: React.FC<RandomImageBlockProps> = (props) => {
     <div className={baseClass}>
       <div className={`${baseClass}__image-container`}>
         {isAnimating && images.map((image, index) => {
-          return <img className={`${baseClass}__image animating`} src={image.src} alt={image.src} data-image-index={index} />
+          return <img key={image.src} className={`${baseClass}__image animating`} src={image.src} alt={image.src} data-image-index={index} />
         })}
 
         {!isAnimating && <img className={`${baseClass}__image current ${isAnimating ? 'animating' : ''}`} src={randomImage.src} alt={randomImage.src} />}
